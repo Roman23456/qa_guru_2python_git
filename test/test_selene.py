66833a456a2efd1af92a3381ca284a4102b1f21e
Roman Semenov <r_semenov@tochka.com>
2026-02-01 15:56:16 +0300

    Тест Allure

---
+++ test/test_selene.py
@@ -0,0 +1,33 @@
+
+from selene.support.conditions import be, have
+from selene.support.shared import browser
+from selene.support.shared.jquery_style import s
+
+def test_github():
+    browser.open("https://github.com")
+
+    # 1. Клик по кнопке поиска (она точно есть)
+    s(".header-search-button").click()
+
+    # 2. Ждём появления модального окна с query builder
+    #    (содержит input с id="query-builder-test")
+    s("#query-builder-test").should(be.visible.and_(be.enabled))
+
+    # 3. Вводим текст напрямую в этот input
+    search_box = s("#query-builder-test")
+    search_box.type("eroshenkoam/allure-playwright-example/allure-example")
+
+    # 4. Нажимаем Enter (или ждём автодополнение + Enter)
+    search_box.press_enter()
+
+    # находим репу и кликаем
+    s('a[href$="eroshenkoam/allure-playwright-example"]').click()
+
+    # ищем и кликаем issues
+    s('#issues-tab').click()
+
+    # сравниваем что issues есть
+    s('//span[contains(., "#1")]')
+
+
+
